43aadde110836ff7e85bfc39ed2d04cf1907a4e9
Tom Fallon <tomfallon9@gmail.com>
2021-04-19 13:31:09 -0400

    index and server js connection update

--- server.js
+++ server.js
@@ -1,35 +1,32 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
-require('dontenv').config();
+const path = require('path')
+require('dotenv').config();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
-
-
-// express middleware
 const app = express();
-app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
 
-// add routes
-app.use(require("./routes/html-routes.js"));
-app.use(require("./routes/api-routes.js"));
-
-// connect to db
-mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/workout",
-    {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
+    
         useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
+        // useUnifiedTopology: true,
+        // useCreateIndex: true,
         useFindAndModify: false
-    }
-);
+    
+});
 
-// Start the server
+
+
+// routes
+app.use(require("./routes/api"));
+app.use(require("./routes/html-routes.js"))
+
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
