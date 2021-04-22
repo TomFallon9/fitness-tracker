95bad8bb0ef1c2ccd6722c0489abc05f0785309a
Tom Fallon <tomfallon9@gmail.com>
2021-04-22 15:51:06 -0400

    changed routing in homeroutes

--- routes/homeroutes.js
+++ routes/homeroutes.js
@@ -1,16 +1,16 @@
-const path = require("path");
-const router = require("express").Router();
+const router = require('express').Router();
+const path = require('path');
 
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "public", "index.html"))
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-router.get('/exercise', (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "public", "exercise.html"))
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../public/stats.html"));
 });
 
-router.get('/stats', (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "public", "stats.html"))
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../public/exercise.html"));
 });
 
 module.exports = router;
