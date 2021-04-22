32adbc3d652ba4145878cead256ecf5c78f8962f
Tom Fallon <tomfallon9@gmail.com>
2021-04-22 15:41:04 -0400

    final connection adjustment

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.URI || "mongodb://localhost/workouts", {
     useNewUrlParser: true,
     useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/api"));
 app.use(require("./routes/homeroutes.js"))
 
 app.listen(PORT, () => {
