b4bd8d8566470e566923a31f3b8fca685b691271
Thomas Fallon <tomfallon9@gmail.com>
2021-04-22 13:53:18 -0400

    Update api.js
    changed the  workouts variable

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const Workout = require("../models/workouts.js");
+const Workout = require("../models/workouts");
 
 
 router.get("/api/workouts/range", (req, res) => {
