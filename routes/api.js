0002c399f878386fece312210924a5549e79bb43
Tom Fallon <tomfallon9@gmail.com>
2021-04-22 05:58:54 -0400

    fixed broken route from Workout to "workouts"

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const Workout = require("../models/Workout");
+const Workout = require("../models/workouts.js");
 
 
 router.get("/api/workouts/range", (req, res) => {
